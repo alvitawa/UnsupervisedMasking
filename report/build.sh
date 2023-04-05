732c671ba19a3261ac3695ce8717d73df8dc15e8
Alfonso Taboada Warmerdam <avtwarmerdam@gmail.com>
2023-04-05 10:44:13 +0200

    subm

--- report/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-docker build -t thesis . --progress=plain && docker run --rm --entrypoint cat thesis /app/title-page-ai.pdf > ../title-page-ai.pdf && docker run --rm --entrypoint cat thesis /app/neurips_2022.pdf > ../neurips_2022.pdf
-
